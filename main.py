ef03e2ba86d5191d2b2496cc7965ca9ff5d73900
Wardstein <jonaseberhard@web.de>
2026-02-05 20:00:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+
 if __name__ == "__main__":
-	if sys.version_info < (3,10):
-		sys.exit('Sorry, Python < 3.10 is not supported')
-	
-	echo "Hello Lab-Box :)"
+        if sys.version_info < (3,10):
+                sys.exit('Sorry, Python < 3.10 is not supported')
+
+        print("Hello Lab-Box :)")
+        input("Press Enter to continue...")
