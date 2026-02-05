6762be23b0655a69f41d781b13a359460c6a386e
Wardstein <jonaseberhard@web.de>
2026-02-05 21:11:58 +0100

    Update startRasPiDisplayModule.sh

--- startRasPiDisplayModule.sh
+++ startRasPiDisplayModule.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# First change into the directory this script lies in
+cd $(dirname "$0")
+# Source the venv name we specified
 source venv-name
+# Run the software
 $LAB_BOX_GUI_VENV/bin/python main.py --isRaspberryPiDisplayModule --withWebserver
